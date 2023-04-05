ff84f96ca5c0f144b47944787651d3bada3ddd90
benminer <bdminer96@gmail.com>
2023-04-05 09:51:19 -0500

    Reference WS URL from params, add build output

--- templates/websockets/vite.config.js
+++ templates/websockets/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { params } from "@ampt/sdk";
 
-process.env.VITE_AMPT_WS_URL =
-  params("AMPT_URL").replace("https", "wss") + "/_ws";
+process.env.VITE_AMPT_WS_URL = params("AMPT_WS_URL");
 
 // https://vitejs.dev/config/
 export default defineConfig({
