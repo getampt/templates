6e19a5c36aeffcf03f6f52ae234fee5f68eceac1
Ben Miner <bdminer96@gmail.com>
2023-04-26 12:07:50 -0500

    Update WebSocket template to remove suffix (#9)

--- templates/websockets/vite.config.js
+++ templates/websockets/vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { params } from "@ampt/sdk";
 
-// If on older version of @ampt/sdk - AMPT_WS_URL will not be available.
-process.env.VITE_AMPT_WS_URL =
-  params("AMPT_WS_URL") || params("AMPT_URL").replace("https", "wss") + "/_ws";
+process.env.VITE_AMPT_WS_URL = params("AMPT_URL").replace("https", "wss");
 
 // https://vitejs.dev/config/
 export default defineConfig({
